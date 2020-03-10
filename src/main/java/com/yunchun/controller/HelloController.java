71d248710acc4675a3b3e801557e53ce64cf236d
yunchun <yunchun@tist.com.tw>
2020-03-10 18:03:54 +0800

    first commit

---
+++ src/main/java/com/yunchun/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.yunchun.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+@Controller("com.yunchun.controller.HelloController")
+public class HelloController {
+
+    @GetMapping("/hello")
+    //@RequestMapping (value = "/hello", method = {RequestMethod.GET,RequestMethod.POST})
+    public String Hello(Model model,@RequestParam("name") String name) {
+        System.out.println(name);
+        model.addAttribute("name",name);
+        return "Hello";
+    }
+}
---
+++ src/test/java/com/yunchun/SpringBoot1stApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yunchun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBoot1stApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
